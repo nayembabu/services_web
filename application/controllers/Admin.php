153f01230c014620be2a22276e4ee830663f31f9
nayembabu <babunayem@gmail.com>
2020-02-14 21:39:52 +0600

    service Commit

---
+++ application/controllers/Admin.php
@@ -0,0 +1,31 @@
+<?php 
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/*
+ * Services Web
+ * Author : Nayem Babu
+ * FB Link : https://www.facebook.com/nayem.b
+ */
+
+class Admin extends CI_Controller {
+	
+	function __construct() {
+		parent::__construct();
+        $this->load->library('session');
+        $this->load->library('form_validation');
+        $this->load->library('Upload');
+        $this->load->library('pdf');
+	}
+
+	function index() {
+		echo "Work";
+	}
+
+	function login() {
+		echo "login page";
+	}
+
+	function sign_up() {
+		echo "Signup Page";
+	}
+}
---
+++ application/controllers/Login.php
@@ -0,0 +1,33 @@
+<?php 
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/*
+ * Services Web
+ * Author : Nayem Babu
+ * FB Link : https://www.facebook.com/nayem.b
+ */
+
+class Login extends CI_Controller {
+	
+	function __construct() {
+		parent::__construct();
+        $this->load->library('session');
+        $this->load->library('form_validation');
+        $this->load->library('Upload');
+        $this->load->library('pdf');
+	}
+
+	function index() {
+		$this->load->view('home/headr');
+		$this->load->view('home/home');
+		$this->load->view('home/footr');
+	}
+
+	function login() {
+		echo "login page";
+	}
+
+	function sign_up() {
+		echo "Signup Page";
+	}
+}
